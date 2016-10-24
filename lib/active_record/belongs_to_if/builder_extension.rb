f4cc0533d99ea2f590703e20f1590dea8e51c07c
Takashi Nakagawa <tak1240@gmail.com>
2016-10-24 20:03:14 +0900

    Fix methods to get compatibility with Rails5

--- lib/active_record/belongs_to_if/builder_extension.rb
+++ lib/active_record/belongs_to_if/builder_extension.rb
@@ -1,8 +1,21 @@
 module ActiveRecord
   module BelongsToIf
     module BuilderExtension
+      # To keep compatibility with Rails > 4
       def valid_options
         super + [:if]
+      end
+
+      module ClassMethods
+        def valid_options(options)
+          super + [:if]
+        end
+      end
+
+      def self.prepended(base)
+        class << base
+          prepend ClassMethods
+        end
       end
     end
   end

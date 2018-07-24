7b84db2f397c48d90576eab781d0d41b224b7c9b
NaturalHokke <natural.hokke@gmail.com>
2018-07-24 16:21:01 +0900

    Support ActiveRecord 5.2.0

--- lib/active_record/belongs_to_if/extend.rb
+++ lib/active_record/belongs_to_if/extend.rb
@@ -1,4 +1,8 @@
 ActiveSupport.on_load(:active_record) do
   ActiveRecord::Associations::Builder::BelongsTo.send(:prepend, ActiveRecord::BelongsToIf::BuilderExtension)
-  ActiveRecord::Associations::Preloader::BelongsTo.send(:prepend, ActiveRecord::BelongsToIf::PreloaderExtension)
+  if ActiveRecord.gem_version >= Gem::Version.new('5.2.0')
+    ActiveRecord::Associations::Preloader::Association.send(:prepend, ActiveRecord::BelongsToIf::PreloaderExtension)
+  else
+    ActiveRecord::Associations::Preloader::BelongsTo.send(:prepend, ActiveRecord::BelongsToIf::PreloaderExtension)
+  end
 end
